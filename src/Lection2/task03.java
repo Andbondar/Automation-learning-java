aae57a7e25a52eaf0ec9c08e9734c82e35850103
andbondar <andbondar@yandex.ru>
2015-03-31 00:33:26 +0300

    lection2 task03 added

---
+++ src/Lection2/task03.java
@@ -0,0 +1,13 @@
+package Lection2;
+
+/**
+ * Created by User on 31.03.2015.
+ */
+public class task03 {
+    public static void main(String[] args) {
+        for(int i = 1; i < 100; i++){
+            if (i%2 == 0) continue;
+            System.out.println(i);
+        }
+    }
+}
