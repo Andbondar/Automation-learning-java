353e5bf5c9dc35ebc4d23c54c2ecac8a6c4b7119
andbondar <andbondar@yandex.ru>
2015-03-31 00:36:50 +0300

    lection2 task04 added

---
+++ src/Lection2/task04.java
@@ -0,0 +1,14 @@
+package Lection2;
+
+/**
+ * Created by User on 31.03.2015.
+ */
+public class task04 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 100){
+            if (i%2 != 0) System.out.println(i);
+            i++;
+        }
+    }
+}
