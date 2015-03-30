532438d4e638d0ba046eff94f28bf6eb9caaf9fe
andbondar <andbondar@yandex.ru>
2015-03-31 01:07:55 +0300

    lection2 task05 added (strange exception here) //TODO: fix

---
+++ src/Lection2/task05.java
@@ -0,0 +1,25 @@
+package Lection2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by User on 31.03.2015.
+ */
+public class task05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0, add = 0;
+        String userInput = "0";
+
+        while (!("сумма".equals(userInput))){
+            add = Integer.parseInt(userInput);
+            sum = sum+add;
+            System.out.println(sum);
+            userInput = reader.readLine();
+        }
+        System.out.println(sum);
+    }
+}
