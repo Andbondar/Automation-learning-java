a937b0eaabbca29b1e8dc9311aa163e0985ff68e
Andbondar <andbondar@yandex.ru>
2015-03-30 14:29:47 +0300

    test commit

--- src/TypeConversion.java
+++ src/TypeConversion.java
@@ -5,7 +5,7 @@ import java.io.UnsupportedEncodingException;
  */
 public class TypeConversion {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        //String to byte
+            //String to byte
         Byte b1 = new Byte("10");
         System.out.println(b1);
 
@@ -24,7 +24,8 @@ public class TypeConversion {
         String s = new String(b3, "cp1251");
         System.out.println(s);
 
-        //String to short
+
+            //String to short
         System.out.println();
         Short s1 = new Short("68");
         System.out.println(s1);
@@ -34,5 +35,52 @@ public class TypeConversion {
 
         short s3 = Short.parseShort(String.valueOf(s1));
         System.out.println(s3);
+
+
+            //String ot int
+        System.out.println();
+        Integer myInt = new Integer("123456");
+        System.out.println(myInt);
+
+        String myString = "54312";
+        System.out.println(Integer.valueOf(myString));
+        System.out.println(Integer.parseInt(myString));
+
+
+            //String to long
+        System.out.println();
+        Long myLong = new Long("98765");
+        System.out.println(myLong);
+        myString = "876345";
+        System.out.println(Long.valueOf(myString));
+        System.out.println(Long.parseLong(myString));
+
+
+            //String to float
+        System.out.println();
+        Float myFloat = new Float("12.25");
+        System.out.println(myFloat);
+        myString = "23.48";
+        System.out.println(Float.valueOf(myString));
+        System.out.println(Float.parseFloat(myString));
+
+
+            //String to Double
+        System.out.println();
+        Double myDouble = new Double("14.23e12");
+        System.out.println(myDouble);
+        myString = "14.37e5";
+        System.out.println(Double.valueOf(myString));
+        System.out.println(Double.parseDouble(myString));
+
+
+            //String to boolean
+        System.out.println();
+        Boolean myBool = new Boolean("True");
+        System.out.println(myBool);
+        myString = "true";
+        System.out.println(Boolean.valueOf(myBool));
+
+
     }
 }
