ff8cebd94990376203c713ffbada47b702e2b1ae
Andbondar <andbondar@yandex.ru>
2015-03-31 22:52:04 +0300

    regexpressions list constructor logic added

--- src/Lection2/Additional_regexp_extractor/DateExtractor.java
+++ src/Lection2/Additional_regexp_extractor/DateExtractor.java
@@ -1,5 +1,6 @@
 package Lection2.Additional_regexp_extractor;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -15,6 +16,20 @@ public class DateExtractor {
 
     }
 
+    //list of regular expressions used for string parsing
+    public List<String> regExpressionsList(){
+        List<String> regExpressions = new ArrayList<String>();
+        regExpressions.add("[0-9]{1,2}-\\w{3}-[0-9]{4}");//DD-MON-YYYY, D-MON-YYYY
+        regExpressions.add("[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}");//DD-MM-YYYY, D-M-YYYY
+        regExpressions.add("[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{2,4}");//DD.MM.YYYY, D.M.YYYY, DD.MM.YY
+        regExpressions.add("[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}");//YYYY-MM-DD, YYYY-M-D
+        regExpressions.add("[A-Z]\\w* [0-9]{1,2}, [0-9]{4}");//March 12, 2008
+        regExpressions.add("Q[1-4] [0-9]{4}");//Q4 2014
+        regExpressions.add("[0-9] Q[1-4]");//2012 Q3
+
+        return regExpressions;
+    }
+
     //Simple date extractor for specified date format YYYY-MM-DD
     public static String extractDate(String inputString){
         Pattern pattern = Pattern.compile("[0-9]{1,4}[.-][[0-9]\\w]{1,3}[.-][0-9]{1,4}");//pattern matches dates. Month can be specified either by number or by 3-letter name like 'JUN'
@@ -25,21 +40,25 @@ public class DateExtractor {
 
     //Advanced Date extractor (by matching regular expressions). First date found in input string is extracted
     public static String getDateFromString(String inputString){
-        List<String> regExpressions = new ArrayList<String>();
-        regExpressions.add("[0-9]{1,2}-\\w{3}-[0-9]{4}");//DD-MON-YYYY, D-MON-YYYY
-        regExpressions.add("[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}");//DD-MM-YYYY, D-M-YYYY
-        regExpressions.add("[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{2,4}");//DD.MM.YYYY, D.M.YYYY, DD.MM.YY
-        regExpressions.add("[A-Z]\\w* [0-9]{1,2}, [0-9]{4}");//March 12, 2008
-        regExpressions.add("[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}");//YYYY-MM-DD, YYYY-M-D
-        regExpressions.add("Q[1-4] [0-9]{4}");//Q4 2014
-        regExpressions.add("[0-9] Q[1-4]");//2012 Q3
-
+        List<String> regExpressions = new DateExtractor().regExpressionsList();
         for (String regExpression : regExpressions) {
             Pattern pattern = Pattern.compile(regExpression);//check string by all regexps. If date is found - return it
             Matcher matcher = pattern.matcher(inputString);
             if (matcher.find()) return matcher.group();
         }
         return "No date found";
+    }
+
+    //Advanced Date extractor and unifier
+    public static String getUnifiedDateFromString(String inputString){
+        for (int i = 0; i < 4; i++){
+            //Pattern pattern = Pattern.compile();//check string by all regexps. If date is found - return it
+            //Matcher matcher = pattern.matcher(inputString);
+        }
+
+
+
+        return "no date found";
     }
 }
 
