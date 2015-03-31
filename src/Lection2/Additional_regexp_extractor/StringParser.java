a7c854ce008ebfc34e6aee69584732a202cbafdd
Andbondar <andbondar@yandex.ru>
2015-03-31 16:11:11 +0300

    date parser created

--- src/Lection2/Additional_regexp_extractor/StringParser.java
+++ src/Lection2/Additional_regexp_extractor/StringParser.java
@@ -1,6 +1,9 @@
 package Lection2.Additional_regexp_extractor;
 
-import Lection2.task02.DateGenerator;
+import Lection2.task02.DateGenerator;//will be used as direct pth to class. Just for learning
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by bondar on 3/31/2015.
@@ -9,7 +12,13 @@ public class StringParser {
     public static void main(String[] args) {
         String characterName = StringGenerator.GenerateOneName();//Random character name is generated
         String birthDate = Lection2.task02.DateGenerator.getDatewithInterval(1950, 1990);//random birth date is generated. Date generator from task2 is used
-        System.out.println(characterName + " was born " + birthDate);
+
+        String analyzedString = characterName + " was born " + birthDate;
+        System.out.println(analyzedString);
+
+        Pattern pattern = Pattern.compile("[0-9]{1,4}[.-][[0-9]\\w]{1,3}[.-][0-9]{1,4}");//pattern matches dates. Month can be specified either by number or by 3-letter name like 'JUN'
+        Matcher matcher = pattern.matcher(analyzedString);
+        if (matcher.find()) System.out.println("Date: " + matcher.group());
     }
 }
 
