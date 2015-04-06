40fb2d556e3825d45bea9bbd777767ee2b1f691c
Andbondar <andbondar@yandex.ru>
2015-04-06 12:26:21 +0300

    lection4.task03 added

--- src/Lection4/Task03.java
+++ src/Lection4/Task03.java
@@ -1,7 +1,6 @@
 package Lection4;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Created by bondar on 4/3/2015.
@@ -11,17 +10,40 @@ import java.util.HashMap;
  */
 public class Task03 {
     public static void main(String[] args) {
-        ArrayList<HashMap<String, String>> nameSurnameMap= new ArrayList<HashMap<String, String>>();
-        HashMap<String, String> mapData = new HashMap<String, String>();
-        mapData.put("Name", "Surname");
-        mapData.put("Name2", "Surname2");
-        mapData.put("Name", "Surname3");
-        nameSurnameMap.add(mapData);
+        String[][] myArray = new String[10][2];
+        Map<String, String> mapData = new HashMap<String, String>();
+        mapData.putIfAbsent("Surname", "Name");
+        mapData.putIfAbsent("Surname2", "Name2");
+        mapData.putIfAbsent("Surname3", "Name3_Unique");
+        mapData.putIfAbsent("Surname4", "Name4");
+        mapData.putIfAbsent("Surname5", "Name");
+        mapData.putIfAbsent("Surname6", "Name2");
+        mapData.putIfAbsent("Surname7", "Name2");
+        mapData.putIfAbsent("Surname8", "Name4");
+        mapData.putIfAbsent("Surname9", "NameUnique");
+        mapData.putIfAbsent("Surname10", "AnotherUniqueName");
 
         System.out.println(mapData.toString());
-        System.out.println(nameSurnameMap);
+        Set<String> mapKeys = mapData.keySet();
+        ArrayList<String> keysToBeRemoved = new ArrayList<String>();
 
+        //here we create list of keys that should be removed from the map
+        for (int i = 0; i < mapKeys.toArray().length; i++){
+            int valuesCount = 0;
+            String currentValue = mapData.get(mapKeys.toArray()[i]);
+            for (String value: mapData.values()) {//check if name is present multiple times
+                if (value.equals(mapData.get(mapKeys.toArray()[i]))) valuesCount++;
+            }
 
-        //find some more infor here. Intersting solution
+            if (valuesCount > 1){
+                keysToBeRemoved.add(mapKeys.toArray()[i].toString());//create arraylist of keys that should be removed from map
+            }
+        }
+
+        //here we remove keys from the map
+        for (int i = 0; i < keysToBeRemoved.size(); i++){//remove entries from map via existing key list
+            mapData.remove(keysToBeRemoved.get(i));
+        }
+        System.out.println(mapData);
     }
 }
