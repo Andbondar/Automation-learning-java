ccab2e08c3b7551569cd2391c8f06b9da31cbed0
Andbondar <andbondar@yandex.ru>
2015-04-07 18:20:16 +0300

    upd for lection5 task

---
+++ src/Lection5/StringSplitting.java
@@ -0,0 +1,27 @@
+package Lection5;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+import java.util.Arrays;
+
+/**
+ * Created by bondar on 4/7/2015.
+ */
+public class StringSplitting {
+    public static void main(String[] args) {
+
+    }
+    public static void splitString(String inputString){
+        System.out.println("String splitted into String array: " + Arrays.toString(Splitter.on(", ").splitToList(inputString).toArray()));
+    }
+    public static void splitStingWOEmptyStrings(String inputString){
+        System.out.println("String splitted into String array (w/o empty Strings): " + Arrays.toString(Splitter.on(", ").omitEmptyStrings().splitToList(inputString).toArray()));
+    }
+    public static void splitStringAndTrimSpaces(String inputString){
+        System.out.println("String splitted into String array (spaces trimmed): " + Arrays.toString(Splitter.on(", ").trimResults(CharMatcher.WHITESPACE).splitToList(inputString).toArray()));
+    }
+    public static void splitStringAndTrimNumbers(String inputString){
+        System.out.println("String splitted into String array (numbers trimmed): " + Arrays.toString(Splitter.on(", ").trimResults(CharMatcher.inRange('0', '9')).splitToList(inputString).toArray()));
+    }
+}
